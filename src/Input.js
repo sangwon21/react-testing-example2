e5105468de4a07967274af4f31b47d9446ad20ad
sangwon <junothyme@gmail.com>
2020-10-25 23:21:30 +0900

    feat: Tested Input component

--- src/Input.js
+++ src/Input.js
@@ -1,12 +1,18 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+import successContext from "./contexts/successContext";
 import languageContext from "./contexts/languageContext";
 import stringsModule from "./helpers/strings";
 
-const Input = ({ secretWord }) => {
+const Input = () => {
   const language = React.useContext(languageContext);
   const [currentGuess, setCurrentGuess] = React.useState("");
+  const [success, setSuccess] = successContext.useSuccess();
+
+  if (success) {
+    return null;
+  }
 
   return (
     <div data-test="component-input">
--- src/Input.test.js
+++ src/Input.test.js
@@ -2,14 +2,20 @@ import React from "react";
 import { mount } from "enzyme";
 import { findByTestAttr, checkProps } from "../test/testUtils";
 import Input from "./Input";
-import languageContext from "./contexts/languageContext";
 
-const setup = ({ language, secretWord }) => {
+import languageContext from "./contexts/languageContext";
+import successContext from "./contexts/successContext";
+
+const setup = ({ language, secretWord, success }) => {
   language = language || "en";
   secretWord = secretWord || "party";
+  success = success || false;
+
   return mount(
     <languageContext.Provider value={language}>
-      <Input secretWord={secretWord} />
+      <successContext.SuccessProvider value={[success, jest.fn()]}>
+        <Input secretWord={secretWord} />
+      </successContext.SuccessProvider>
     </languageContext.Provider>
   );
 };
@@ -63,4 +69,9 @@ describe("langaugePicker", () => {
 
     expect(submitButton.text()).toBe("🚀");
   });
+});
+
+test("input component does not show when success is true", () => {
+  const wrapper = setup({ secretWord: "party", success: true });
+  expect(wrapper.isEmptyRender()).toBe(true);
 });

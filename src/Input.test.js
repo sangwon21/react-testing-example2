9675478388f2fca5accfd94e56c4a76229b1e8c4
sangwon <junothyme@gmail.com>
2020-10-25 15:17:54 +0900

    feat: Cleared currentGuess on submit

--- src/Input.test.js
+++ src/Input.test.js
@@ -18,16 +18,27 @@ test("does not throw warning with expected props", () => {
 });
 
 describe("state controlled input field", () => {
-  test("state updates with value of input box upon changed", () => {
-    const mockSetCurrentGuess = jest.fn();
+  let mockSetCurrentGuess = jest.fn();
+  let wrapper;
+  beforeEach(() => {
+    mockSetCurrentGuess.mockClear();
     React.useState = jest.fn(() => ["", mockSetCurrentGuess]);
-
-    const wrapper = setup();
+    wrapper = setup();
+  });
+  test("state updates with value of input box upon changed", () => {
     const inputBox = findByTestAttr(wrapper, "input-box");
 
     const mockEvent = { target: { value: "train" } };
     inputBox.simulate("change", mockEvent);
 
     expect(mockSetCurrentGuess).toHaveBeenCalledWith("train");
+  });
+
+  test("field is cleared upon submit button click", () => {
+    const submitButton = findByTestAttr(wrapper, "submit-button");
+
+    submitButton.simulate("click", { preventDefault() {} });
+
+    expect(mockSetCurrentGuess).toHaveBeenCalledWith("");
   });
 });

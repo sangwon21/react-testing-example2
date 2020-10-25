9e5de40bb9fc796b22dda2ca218aa16ce60756d1
sangwon <junothyme@gmail.com>
2020-10-25 21:51:36 +0900

    feat: Tested Congrats component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import React from "react";
 import hookActions from "./actions/hookActions";
 import "./App.css";
-
+import languageContext from "./contexts/languageContext";
+import LanguagePicker from "./LangaugePicker";
 import Input from "./Input";
 
 function reducer(state, action) {
   switch (action.type) {
     case "setSecretWord":
       return { ...state, secretWord: action.payload };
+    case "setLanguage":
+      return { ...state, langauge: action.payload };
     default:
       throw new Error(`Invalid action type: ${action.type}`);
   }
 }
 
 function App() {
-  const [state, dispatch] = React.useReducer(reducer, { secretWord: null });
+  const [state, dispatch] = React.useReducer(reducer, {
+    secretWord: null,
+    langauge: "en",
+  });
 
   const setSecretWord = (secretWord) =>
     dispatch({ type: "setSecretWord", payload: secretWord });
+  const setLanguage = (langauge) =>
+    dispatch({ type: "setLangauge", payload: langauge });
 
   React.useEffect(() => {
     hookActions.getSecretWord(setSecretWord);
@@ -36,7 +44,11 @@ function App() {
 
   return (
     <div className="container" data-test="component-app">
-      <Input secretWord={state.secretWord} />
+      <h1>Jotto</h1>
+      <languageContext.Provider value={state.langauge}>
+        <LanguagePicker setLanguage={setLanguage} />
+        <Input secretWord={state.secretWord} />
+      </languageContext.Provider>
     </div>
   );
 }
